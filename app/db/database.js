ba2a1772b8ee205dd0ce5954e036faae97c7245d
mrqrvly <mrqrvly@gmail.com>
2016-08-25 13:58:52 -0500

    added mlab mongo host considerations

--- app/db/database.js
+++ app/db/database.js
@@ -7,7 +7,8 @@ var mongoose = require('mongoose');
 
 //  Locate the database
 //  -------------------
-var connectionString = 'mongodb://localhost/users';
+var connectionString = process.env.DB_HOST;
+//  'mongodb://localhost/users';
 
 //  Connect to the database
 //  -----------------------
